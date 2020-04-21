ed1092dae4077ed7dba369d8edc56ed67e7aa505
calmesam01 <sanyamsareen01@gmail.com>
2020-04-21 00:49:41 -0400

    Set user schema virtual fields and methods

--- backEnd/models/user.js
+++ backEnd/models/user.js
@@ -51,4 +51,42 @@ const userSchema = new mongoose.Schema({
     }
 }, {timestamps: true})
 
+userSchema
+    .virtual('password')
+    .set(function (password) {
+        //Create a temporary variable called _password
+        this._password = password
+        //Generate salt
+        this.salt = this.makeSalt()
+        //Encrypt password
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(function () {
+        return this._password
+    })
+
+    userSchema.methods = {
+        authenticate: function (plainText) {
+            return this.encryptPassword(plainText) === this.hashed_password
+        },
+
+        encryptPassword: function (password) {
+            if (!password) {
+                return '';
+            }
+            try {
+                return crypto
+                            .createHmac('sha1', this.salt)
+                            .update(password)
+                            .digest('hex');
+            } catch (error) {
+                return error;
+            }
+        },
+
+        makeSalt: function () {
+            return Math.round(new Date().valueOf() * Math.random()) + '';
+        }
+    }
+
 module.exports = mongoose.model('User', userSchema)
