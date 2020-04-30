0a5177103a69417eaa1ab4e9728fbd1ff0f920c0
calmesam01 <sanyamsareen01@gmail.com>
2020-04-30 16:01:37 -0400

    Add the related blog section frontend

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -273,6 +273,7 @@ exports.photo = (req, res) => {
 };
 
 exports.listRelated = (req, res) => {
+    // console.log(req.body.blog);
     let limit = req.body.limit ? parseInt(req.body.limit) : 3;
     const { _id, categories } = req.body.blog;
 
@@ -285,7 +286,7 @@ exports.listRelated = (req, res) => {
                 return res.status(400).json({
                     error: 'Blogs not found'
                 });
-                res.json(blogs);
             }
+            res.json(blogs);
         });
 };
