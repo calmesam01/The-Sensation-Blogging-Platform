a3dcdd386e34ad1d633a978731a78d0dd3e5c207
calmesam01 <sanyamsareen01@gmail.com>
2020-05-04 16:33:15 -0400

    Setup the update blog page

--- frontEnd/pages/admin/crud/[slug].js
+++ frontEnd/pages/admin/crud/[slug].js
@@ -1,19 +1,19 @@
 import Layout from '../../../components/Layout';
 import Admin from '../../../components/auth/Admin';
-import BlogRead from '../../../components/crud/BlogRead';
+import BlogUpdate from '../../../components/crud/BlogUpdate';
 import Link from 'next/link';
 
 const Blog = () => {
     return (
         <Layout>
             <Admin>
-                <div className="container">
+                <div className="container-fluid">
                     <div className="row">
                         <div className="col-md-12 pt-5 pb-5">
-                            <h2>Manage blogs</h2>
+                            <h2>Update blog</h2>
                         </div>
                         <div className="col-md-12">
-                            <BlogRead />
+                            <BlogUpdate />
                         </div>
                     </div>
                 </div>
